6e88faf9b26032a19013608b3d63dba4d744b0c2
Johannes Kuusela <jokuusel@Johanness-MacBook-Air.local>
2015-03-27 11:58:11 +0200

    output

--- prh.py
+++ prh.py
@@ -1,17 +1,16 @@
-import requests, json
+import requests, json, argparse, csv, pymongo
 from datetime import date, timedelta
-from pymongo import MongoClient
 
 
-def main():
-  client = MongoClient()
-  companies_table = client.mydb.companies
+def persist():
+  companies_collection = get_companies_collection()
   last_day = date.today() - timedelta(1)
-  parameters = {'companyRegistrationFrom':last_day.isoformat()}
+  register_date = last_day.isoformat()
+  parameters = {'companyRegistrationFrom':register_date}
   r = requests.get("http://avoindata.prh.fi/tr/tv", params=parameters)
   for result in r.json()['results']:
     business_id = result['businessId']
-    existing = companies_table.find({'business_id':business_id})
+    existing = companies_collection.find({'business_id':business_id})
     if existing.count() != 0:
     	print "business id exists, skipping: "+business_id
     	continue
@@ -23,10 +22,44 @@ def main():
     	               street=address['street'],
     	               post_code=address['postCode'],
     	               city=address['city'],
-    	               website=address['website']
+    	               website=address['website'],
+    	               phone=address['phone'],
+    	               register_date=register_date
     	               )
-    db_id = companies_table.insert(contact_info)
+    db_id = companies_collection.insert(contact_info)
     print 'added company with business id '+business_id
 
+def dump():
+  companies_collection = get_companies_collection()
+  filename = "out-%s.csv"%date.today().isoformat()
+  f = csv.writer(open(filename, "wb+"))
+
+  f.writerow(["pvm", "y-tunnus", "nimi", "osoite", "puhelin" , "url"])  
+  for doc in companies_collection.find().sort('register_date', pymongo.ASCENDING):
+    register_date = doc['register_date'] if doc['register_date'] else ''
+    business_id = doc['business_id'] if doc['business_id'] else ''
+    name = doc['name'] if doc['name'] else ''
+    address = doc['street'] + ' ' +doc['post_code'] + ' ' +doc['city']
+    phone = doc['phone'] if doc['phone'] else ''
+    url =  doc['website'] if doc['website'] else ''
+    f.writerow([
+    	register_date.encode('utf-8'), 
+    	business_id.encode('utf-8'), 
+    	name.encode('utf-8'), 
+    	address.encode('utf-8'), 
+    	phone.encode('utf-8'), 
+    	url.encode('utf-8')])
+
+def get_companies_collection():
+  client = pymongo.MongoClient()
+  return client.mydb.companies
+
 if __name__ == "__main__":
-    main()
+  parser = argparse.ArgumentParser(description='Persists company information from PRH. Use option --dump to output information into csv file.')  
+  parser.add_argument('-d','--dump', help='Dump company information into out-<date>.csv', action='store_true')
+
+  args = parser.parse_args()
+  if  not args.dump:
+    persist()
+  else :
+    dump()
